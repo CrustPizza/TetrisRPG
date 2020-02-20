ed7866836b627034a25e96afb9be489222947df3
final-boss-Teferi <53199927+final-boss-Teferi@users.noreply.github.com>
2020-02-20 14:04:09 +0900

    Update keyManager.h

--- Framework/Manager/keyManager.h
+++ Framework/Manager/keyManager.h
@@ -1,8 +1,5 @@
 #pragma once
 #include "singletonBase.h"
-//=============================================================
-//	## keyManager ## 키매니져
-//=============================================================
 
 #define KEYMAX 256
 
@@ -18,15 +15,9 @@ public:
 	//키매니져 해제
 	void release();
 
-	//GetKey(keycode.)
-	//GetKeyDown()
-	//키가 한번만 눌렸냐?
 	bool isOnceKeyDown(int key);
-	//키가 한번 눌렸다 띄었냐?
 	bool isOnceKeyUp(int key);
-	//키가 계속 눌려 있냐?
 	bool isStayKeyDown(int key);
-	//토글키냐?
 	bool isToggleKey(int key);
 
 	keyManager() {}
