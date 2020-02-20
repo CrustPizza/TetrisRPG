bf2e9ea2b9a1f6fcbf894030db1e2d714886242b
final-boss-Teferi <53199927+final-boss-Teferi@users.noreply.github.com>
2020-02-20 17:29:55 +0900

    Update stdafx.h

--- stdafx.h
+++ stdafx.h
@@ -1,22 +1,18 @@
-﻿// stdafx.h : 자주 사용하지만 자주 변경되지는 않는
-// 표준 시스템 포함 파일 또는 프로젝트 관련 포함 파일이
-// 들어 있는 포함 파일입니다.
-//
 #pragma once
 
 #include <SDKDDKVer.h>
 
-//디버깅용 (주석치면 콘솔창이 사라진다)
+//디버깅용
 //#pragma comment(linker, "/entry:WinMainCRTStartup /subsystem:console")
 
-#define WIN32_LEAN_AND_MEAN             // 거의 사용되지 않는 내용은 Windows 헤더에서 제외합니다.
-// Windows 헤더 파일:
+#define WIN32_LEAN_AND_MEAN             // 거의 사용되지 않는 내용은 Windows 헤더에서 제외
+// Windows 헤더 파일
 #include <Windows.h>
-// C 런타임 헤더 파일입니다.
+// C 런타임 헤더
 #include <stdlib.h>
 #include <stdio.h>
 #include <tchar.h>
-// C++ 런타임 헤더 파일입니다.
+// C++ 런타임 헤더
 #include <iostream>
 using namespace std;
 // 자주사용하는 STL
@@ -25,6 +21,7 @@ using namespace std;
 #include <map>
 #include <list>
 
+// 방향
 namespace dir
 {
 	enum tagDirection
@@ -37,9 +34,7 @@ namespace dir
 	};
 }
 
-//=============================================================
-//	## 내가 만든 헤더파일을 이곳에 추가한다 ##
-//=============================================================
+// 커스텀 헤더
 #include "commonMacroFunction.h"
 #include "keyManager.h"
 #include "randomFunction.h"
@@ -53,37 +48,28 @@ using namespace dir;
 #include "sceneManager.h"
 #include "soundManager.h"
 
-//=============================================================
-//	## 싱글톤을 추가한다 ##
-//=============================================================
+// 싱글톤
 #define KEYMANAGER keyManager::getSingleton()
 #define RND randomFunction::getSingleton()
 #define IMAGEMANAGER imageManager::getSingleton()
-#define PLAYERDATA playerData::getSingleton()
 #define TXTDATA	txtData::getSingleton()
 #define INIDATA iniData::getSingleton()
 #define SCENEMANAGER sceneManager::getSingleton()
 #define SOUNDMANAGER soundManager::getSingleton()
 
-//=============================================================
-//	## 디파인문 ## (윈도우창 초기화)
-//=============================================================
-#define WINNAME (LPTSTR)(TEXT("김지훈_TetrisRPG"))
+// define문
+#define WINNAME 	(LPTSTR)(TEXT("김지훈_TetrisRPG"))
 #define WINSTARTX	600
 #define WINSTARTY	10
 #define WINSIZEX	540
 #define WINSIZEY	960
 #define WINSTYLE	WS_CAPTION | WS_SYSMENU
 
-//=============================================================
-//	## 매크로함수 ## (클래스에서 동적할당된 부분 해제)
-//=============================================================
+// 매크로함수
 #define SAFE_DELETE(p)		{if(p) {delete (p); (p) = NULL;}}
 #define SAFE_DELETE_ARRAY(p)	{if(p) {delete[] (p); (p) = NULL;}}
 
-//=============================================================
-//	## 전역변수 ##
-//=============================================================
+// 전역변수
 extern HINSTANCE	_hInstance;
-extern HWND			_hWnd;
+extern HWND		_hWnd;
 extern POINT		_ptMouse;
