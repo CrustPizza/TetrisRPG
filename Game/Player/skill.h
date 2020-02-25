767a400e9ad3c4278305e5cd6b7d7514ca7220fe
final-boss-Teferi <53199927+final-boss-Teferi@users.noreply.github.com>
2020-02-25 22:40:42 +0900

    Update and rename skill.h to Game/Player/skill.h

--- Game/Player/skill.h
+++ Game/Player/skill.h
@@ -2,12 +2,14 @@
 #include "gameNode.h"
 #include "enemy.h"
 
+// 스킬이름 명시
 enum tagSkillName
 {
 	SLOW,
 	LONG_BLOCK
 };
 
+// 스킬 데이터 구조체
 struct tagSkill
 {
 	tagSkillName name;
@@ -23,26 +25,31 @@ struct tagSkill
 class skill : public gameNode
 {
 private:
+	// 에너미
 	enemy* _enemy;
 
+	// 스킬 슬롯번호
 	tagSkill skill1;
 	tagSkill skill2;
 
 public:
 	HRESULT init();
 	void release();
-	void update();
-	void render();
 
+	// 스킬 게터
 	tagSkill* getSkill1Ptr() { return &skill1; }
 	tagSkill* getSkill2Ptr() { return &skill2; }
 
+	// 스킬 발동
 	void skillActivate(tagSkill* skill, tagStone* enemy, bool* tetris = nullptr);
 
+	// 스킬 렌더
 	void skillRender(tagSkill skill, int x, int y);
 
+	// 상호참조용 에너미 셋 함수
 	void setEnemy(enemy* enemy) { _enemy = enemy; }
 
+	// 스킬 셋
 	void setSkill(tagSkill* skill, tagSkillName name);
 
 	skill() {}
